956b01c8f4d4449727bfb7295f80225f87c55565
Matt Atwell <ma3505@rit.edu>
2018-03-15 15:30:00 -0400

    Initial Commit

---
+++ server.php
@@ -0,0 +1,9 @@
+<!-- PHP REST SERVER for chat App  -->
+<?php
+ header("Access-Control-Allow-Origin: *");
+if($_POST[username] != null){
+  header('Location:http://127.0.0.1:9966/redirect.html');
+}
+
+
+?>
