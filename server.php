027dba6c8731537e20a0663397fd82416915164f
Rixhers Ajazi <CoderRix@gmail.com>
2018-04-09 20:17:41 -0400

    Delete server.php

--- server.php
+++
@@ -1,9 +0,0 @@
-<!-- PHP REST SERVER for chat App  -->
-<?php
- header("Access-Control-Allow-Origin: *");
-if($_POST[username] != null){
-  header('Location:http://127.0.0.1:9966/redirect.html');
-}
-
-
-?>
