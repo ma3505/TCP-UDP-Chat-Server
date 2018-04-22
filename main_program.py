21b8ffa150330ace0eab49466b4cadf460e0115a
Rix <rix@datto.com>
2018-04-22 09:43:53 -0400

    comments

--- main_program.py
+++ main_program.py
@@ -1,11 +1,12 @@
-"""main program to initiate the servers that we care about """
+"""Main program that initiates a UDP and TCP server to handle client communications"""
+
 
 from threading import Thread
-
 import socket
 import re
 
 
+# Dynamically get a unique port and server IP.
 def get_server_ip_and_port():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
@@ -14,6 +15,8 @@ def get_server_ip_and_port():
     return (addr, port)
 
 
+# This method kicks off the TCP server used to handle tcp messages coming from clients.
+# Additionally this will spawn off a incoming connection thread handler.
 def initiate_tcp_server(server):
     print("Initiating TCP Server...")
     host, port = get_server_ip_and_port()
@@ -31,6 +34,8 @@ def initiate_tcp_server(server):
         server = None
 
 
+# This method kicks off the UDP server used to handle tcp messages coming from clients.
+# Additionally this will spawn off a incoming connection thread handler.
 def initiate_udp_server(server):
     print("Initiating UDP Server...")
     host, port = get_server_ip_and_port()
@@ -48,10 +53,21 @@ def initiate_udp_server(server):
         server = None
 
 
+# This method will loop endlessly while it awaits clients to connect to its socket. When it does register the client
+# it will send it back a connected keyword, and then spawn off another thread to handle the new client.
+def handle_incoming_connections(server):
+    while True:
+        client_socket, client_address = server.accept()
+        print("New connection from %s" % str(client_address))
+        client_socket.send("<<<CONNECTED>>>")
+        Thread(target=handle_client_connection, args=(client_socket,)).start()
+
+
+# This function is used
 def handle_client_connection(client):
     # De_encodes names to be used as a variable
     name = str(de_encode(client.recv(BUFFER_SIZE).decode("utf8"))[1])
-    msg = set_encoding("NEW_USER",name + " connected" )
+    msg = set_encoding("NEW_USER", name + " connected" )
     send_all(msg)
 
     CLIENTS[client] = name
@@ -59,32 +75,25 @@ def handle_client_connection(client):
         while True:
             msg = client.recv(BUFFER_SIZE)
             if msg != "<<<EXIT>>>":
-                #  attach new_message encoding with user message
-                user_msg = set_encoding("NEW_MESSAGE",str(name)+": "+str(msg))
+                # attach new_message encoding with user message
+                user_msg = set_encoding("NEW_MESSAGE", str(name)+": "+str(msg))
                 send_all(user_msg)
             else:
                 client.send("<<<EXITED>>>")
                 client.close()
                 del CLIENTS[client]
-                left_msg = set_encoding("LEFT",name)
+                left_msg = set_encoding("LEFT", name)
                 send_all(left_msg)
                 break
     except socket.error:
-        print("Client Disconnected")
+        print("Client Disconnected via error")
 
 
+# This function is responsible for sending all clients regardless of their protocol the new message.
 def send_all(msg):
     for client_socket in CLIENTS:
         client_socket.send(str(msg))
 
-
-
-def handle_incoming_connections(server):
-    while True:
-        client_socket, client_address = server.accept()
-        print("New connection from %s" % str(client_address))
-        client_socket.send("<<<CONNECTED>>>")
-        Thread(target=handle_client_connection, args=(client_socket,)).start()
 
 # Function To Handle the Delimiter using Regex
 def de_encode(encoded_msg):
@@ -95,11 +104,9 @@ def de_encode(encoded_msg):
 # Used for encoding messages sent to the client
 # Identifiers are used to communicate messages, userlists, and events
 # between the backend and front-end
-def set_encoding(key_identifer,encoded_msg):
+def set_encoding(key_identifer, encoded_msg):
     result = "<<<" + key_identifer + ">>>" + "<<<" + encoded_msg + ">>>"
     return result
-
-
 
 
 CLIENTS = {}
